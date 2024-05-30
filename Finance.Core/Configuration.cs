f4de1de9f8d87c2ac189e201e6ccdb8e9b21201a
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2024-05-30 07:25:07 -0300

    Ajuste blazor com mudBlazor

--- Finance.Core/Configuration.cs
+++ Finance.Core/Configuration.cs
@@ -7,6 +7,6 @@
         public const int DefaultPageSize = 25;
 
         public static string BackendUrl { get; set; } = string.Empty;
-        public static string FrontendUrl { get; set; } = string.Empty;
+        public static string FrontendUrl { get; set; } = "https://localhost:5201";
     }
 }
